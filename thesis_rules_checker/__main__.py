c64c4a25711c33ce233f1bc76089c140745dbe31
Anxeal <anxealous@gmail.com>
2022-11-13 15:50:58 +0300

    Switch to PyMuPDF library

--- thesis_rules_checker/__main__.py
+++ thesis_rules_checker/__main__.py
@@ -1,7 +1,8 @@
 import argparse
 import sys
-from pprint import pprint
-from PyPDF2 import PdfReader
+import fitz
+
+from fitz import Point
 
 parser = argparse.ArgumentParser(
     prog="Thesis Rules Checker",
@@ -13,37 +14,68 @@ parser.add_argument(
     help="The path of the file to be checked",
     required=False)
 
+parser.add_argument(
+    "-o", "--output",
+    metavar="FILE",
+    help="The path of the output file",
+    required=False)
+
 
 def main() -> int:
     args = parser.parse_args()
 
-    filename = args.input
+    input_file = args.input
 
-    while filename is None:
-        filename = input("Enter the path of the file to be checked: ")
+    while input_file is None:
+        input_file = input("Enter the path of the file to be checked: ")
 
-    try:
-        reader = PdfReader(filename)
-    except FileNotFoundError:
-        print("File not found", file=sys.stderr)
-        return 1
+    output_file = args.output
 
-    for page in reader.pages:
-        page.extract_text(visitor_text=visitor_text)
+    if output_file is None:
+        parts = input_file.split(".")
+        if len(parts) >= 2:
+            parts[-2] += "-checked"
+        else:
+            parts[0] += "-checked"
+        output_file = ".".join(parts)
+
+    document = fitz.open(input_file)
+
+    toc = document.get_toc()
+    toc.append([1, 'Rule Violations', 1])
+
+    for page_index, page in enumerate(document):
+        text_info = page.get_text("dict")
+        toc.append([2, 'Page %d' % (page_index + 1), page_index + 1])
+
+        added_toc = False
+
+        for block in text_info["blocks"]:
+            if "lines" not in block:
+                continue
+            for line in block["lines"]:
+                for span in line["spans"]:
+                    if not approximately_equals(span["size"], 12, 0.1):
+                        page.add_highlight_annot(span["bbox"])
+                        page.add_text_annot((span["bbox"][0] - 20, span["bbox"][1]), "Font size is not 12")
+                        toc.append([3, "Font size is not 12", page_index + 1,
+                                    {"kind": 1,
+                                     "to": Point(span["bbox"][0], span["bbox"][1]),
+                                     "color": (1, 1, 0),
+                                     "bold": True}])
+                        added_toc = True
+
+        if not added_toc:
+            toc.pop()
+
+    document.set_toc(toc)
+    document.ez_save(output_file)
 
     return 0
 
 
-def visitor_text(text, cm, tm, fontDict, fontSize):
-    if text is None or fontDict is None:
-        return
-    print("Text: " + str(text))
-    pprint(cm)
-    pprint(tm)
-    print("x: {}, y: {}".format(tm[4], tm[5]))
-    print("fontDict: " + str(fontDict["/BaseFont"]))
-    print("fontSize: " + str(fontSize))
-    input()
+def approximately_equals(a, b, epsilon=1e-5):
+    return abs(a - b) < epsilon
 
 
 if __name__ == "__main__":
