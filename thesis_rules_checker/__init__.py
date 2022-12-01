1ad4acaee2a37b03c592b7e8924f6fefcff839ba
Anxeal <anxealous@gmail.com>
2022-12-01 17:25:42 +0300

    Extract add_rule_violation method

--- thesis_rules_checker/__init__.py
+++ thesis_rules_checker/__init__.py
@@ -19,24 +19,30 @@ def process_document(document: fitz.Document):
 
         added_toc = False
 
+        # Check font size for all text blocks
         for block in text_info["blocks"]:
             if "lines" not in block:
                 continue
             for line in block["lines"]:
                 for span in line["spans"]:
                     if not approximately_equals(span["size"], 12, 0.1):
-                        page.add_highlight_annot(span["bbox"])
-                        page.add_text_annot((span["bbox"][0] - 20, span["bbox"][1]), "Font size is not 12")
-                        toc.append([3, "Font size is not 12", page_index + 1,
-                                    {"kind": 1,
-                                     "to": fitz.Point(span["bbox"][0], span["bbox"][1]),
-                                     "color": (1, 1, 0),
-                                     "bold": True}])
+                        add_rule_violation("Font size is not 12", span["bbox"], page, page_index, toc)
                         added_toc = True
 
         if not added_toc:
             toc.pop()
     document.set_toc(toc)
+
+
+def add_rule_violation(message, bounding_box, page, page_index, toc):
+    x, y = bounding_box, bounding_box
+    page.add_highlight_annot(bounding_box)
+    page.add_text_annot((x - 20, y), message)
+    toc.append([3, message, page_index + 1,
+                {"kind": 1,
+                 "to": fitz.Point(x, y),
+                 "color": (1, 1, 0),
+                 "bold": True}])
 
 
 def approximately_equals(a, b, epsilon=1e-5):
